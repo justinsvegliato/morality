539e1d5ecbbe430a0559d7484ff7c95e57f0879b
Justin Svegliato <justin.svegliato@gmail.com>
2019-09-28 22:24:21 -0400

    Removed the tolerance function

--- src/ethical-contexts/forbidden-state-ethical-context.js
+++ src/ethical-contexts/forbidden-state-ethical-context.js
@@ -3,15 +3,10 @@
 class ForbiddenStateEthicalContext {
   constructor(forbiddenStates, toleranceFunction) {
     this._forbiddenStates = forbiddenStates;
-    this._toleranceFunction = toleranceFunction;
   }
 
   get forbiddenStates() {
     return this._forbiddenStates;
-  }
-
-  get toleranceFunction() {
-    return this._toleranceFunction;
   }
 }
 
