4f8de1ea4b80f1aeaf2a8fce2cc761a2aaf07fcd
Justin Svegliato <justin.svegliato@gmail.com>
2019-10-05 13:28:36 -0400

    Implemented the first iteration of norm-based ethics

--- src/ethics/norm-based-ethics.js
+++ src/ethics/norm-based-ethics.js
@@ -1,8 +1,9 @@
 'use strict';
 
 class NormBasedEthics {
-  constructor(norms, penaltyFunction, toleranceFunction) {
+  constructor(norms, violationFunction, penaltyFunction, toleranceFunction) {
     this._norms = norms;
+    this._violationFunction = violationFunction;
     this._penaltyFunction = penaltyFunction;
     this._toleranceFunction = toleranceFunction;
   }
@@ -11,12 +12,41 @@ class NormBasedEthics {
     return this._norms;
   }
 
+  get violationFunction() {
+    return this._violationFunction;
+  }
+
   get penaltyFunction() {
     return this._penaltyFunction;
   }
 
   get toleranceFunction() {
     return this._toleranceFunction;
+  }
+
+  transform(agent, program) {
+    for (const constraintState of agent.states()) {
+      program.constraints['normBasedEthics' + constraintState] = {'max': this.toleranceFunction(constraintState)};
+    }
+
+    for (const variableState of agent.states()) {
+      for (const variableAction of agent.actions()) {
+        for (const constraintState of agent.states()) {
+          if (variableState != constraintState) {
+            program.variables['state' + variableState + variableAction]['normBasedEthics' + constraintState] = 0;
+            continue;
+          }
+
+          let coefficient = 0;
+          for (const successorState of agent.states()) {
+            for (const norm of this._violationFunction(successorState)) {
+              coefficient += agent.transitionFunction(variableState, variableAction, successorState) * this._penaltyFunction(norm, variableState, variableAction);
+            }
+          }
+          program.variables['state' + variableState + variableAction]['normBasedEthics' + constraintState] = coefficient;
+        }
+      }
+    }
   }
 }
 
