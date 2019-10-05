f40f86b3cb45410cc77879dfec1ae31c89db0fc5
Justin Svegliato <justin.svegliato@gmail.com>
2019-10-05 10:55:41 -0400

    Restructured example for simplicity

--- src/example.js
+++ src/example.js
@@ -6,21 +6,23 @@ const ForbiddenStateEthics = require('./ethics/forbidden-state-ethics');
 const helper = require('./utils/helper.js');
 const printer = require('./utils/printer.js');
 
-console.log('Grid World');
 const gridWorld = helper.getJson('grid-worlds/6x10-grid-world.json');
-printer.printGridWorld(gridWorld);
 
 const agent = new GridWorldAgent(gridWorld);
 const ethics = new ForbiddenStateEthics([2]);
 
-console.log('Amoral Policy');
 const amoralPolicy = morality.solve(agent);
+const moralPolicy = morality.solve(agent, ethics);
+
+console.log('Grid World');
+printer.printGridWorld(gridWorld);
+
+console.log('Amoral Policy');
 if (amoralPolicy) {
   printer.printPolicy(amoralPolicy, gridWorld);
 }
 
 console.log('Moral Policy');
-const moralPolicy = morality.solve(agent, ethics);
 if (moralPolicy) {
   printer.printPolicy(moralPolicy, gridWorld, ethics);
 }
