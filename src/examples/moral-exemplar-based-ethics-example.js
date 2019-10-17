979ae2eb8c5e59e22b6a48f3211c981e480461be
Justin Svegliato <justin.svegliato@gmail.com>
2019-10-17 18:04:51 -0400

    Renamed stuff

--- src/examples/moral-exemplar-based-ethics-example.js
+++ src/examples/moral-exemplar-based-ethics-example.js
@@ -2,18 +2,17 @@
 
 const morality = require('../morality.js');
 const GridWorldAgent = require('../agents/grid-world-agent.js');
-const MoralExemplarEthics = require('../ethics/moral-exemplar-based-ethics.js');
+const MoralExemplarEthics = require('../ethics/moral-exemplar-ethics.js');
 const helper = require('../utils/helper.js');
 const printer = require('../utils/printer.js');
 
 const gridWorld = helper.getJson('grid-world.json');
 const agent = new GridWorldAgent(gridWorld);
 
-// The argument to the moral exemplar ethics class is a list of pairs of lists.
-// Each pair of lists is a trajectory [s_1, s_2, ..., s_n], [a_1, a_2, ..., a_n]
-// where the exemplar took action a_i when it was in state s_i
-
-const ethics = new MoralExemplarEthics([[[1], ['NORTH']], [[2], ['NORTH']]]);
+const ethics = new MoralExemplarEthics([
+  [[1], ['NORTH']],
+  [[2], ['NORTH']]
+]);
 
 console.log('Domain');
 printer.printDomain(gridWorld, ethics);
