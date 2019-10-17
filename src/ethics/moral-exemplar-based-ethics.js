b787edb76c310d1888726244294a3265c41c024b
SamerNashed <samernashed@gmail.com>
2019-10-17 00:47:02 -0700

    Moral exemplar example runs on 2x2 grid world

--- src/ethics/moral-exemplar-based-ethics.js
+++ src/ethics/moral-exemplar-based-ethics.js
@@ -12,29 +12,52 @@ class MoralExemplarEthics {
 
   transform(agent, program) {
     
-//I'm sorry this is so incomplete :( ... I traveled to LA this weekend.
-
-    program.constraints[`MoralExemplarEthics${state}${action}`] = {'max': 0};
 
     // For every state
     for (const state of agent.states()) {
-        
+     
+      // Add constraints to state action space
+      for (const action of agent.actions()) { // May in general depend on state... TODO
+        program.constraints[`moralExemplarEthics${state}${action}`] = {'max': 0};
+      }
+
+      let morally_permitted_actions = {}
+
       // Look for a corresponding moral example state
       for (const trajectory of this._exemplarTrajectories) {
-        
-        //let is_morally_justified = false;
-        for (const exemplarState of trajectory.states()) {
-     
-          for (const action of agent.actions()) { // May in general depend on state
-            if (action == moralexemplar action) {
-              
-            }
+        let trajectory_index = 0;
+        for (const exemplarState of trajectory[0]) {
+    
+          // Add to permitted actions for this state 
+          if (exemplarState == state) {
+            let exemplarAction = trajectory[1][trajectory_index];
+            morally_permitted_actions[`MoralExemplarEthics${state}${exemplarAction}`] = true;
+          }
+          trajectory_index = trajectory_index + 1;
+        }
+      }
+    
+      // If we have any moral examples
+      if (Object.keys(morally_permitted_actions).length > 0) {
+
+        // Add non-zero coefficients to states where other actions were preferred by exemplars
+        for (const action of agent.actions()) { // May in general depend on state... TODO
+          if (morally_permitted_actions[`MoralExemplarEthics${state}${action}`] != true) {
+            program.variables[`state${state}${action}`][`moralExemplarEthics${state}${action}`] = 1.0;
+          }
+          else { // TODO: Is this necessary?
+            program.variables[`state${state}${action}`][`moralExemplarEthics${state}${action}`] = 0.0;
           }
         }
-        if (is_morally_justified) {
-          //add constraint
+      }
+      else { // Set zero coefficients for all states with no moral examples
+        for (const action of agent.actions()) { // May in general depend on state... TODO
+          program.variables[`state${state}${action}`][`moralExemplarEthics${state}${action}`] = 0.0;
         }
       }
     }
   }
 }
+
+module.exports = MoralExemplarEthics;
+
---
+++ src/examples/moral-exemplar-based-ethics-example.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const morality = require('../morality.js');
+const GridWorldAgent = require('../agents/grid-world-agent.js');
+const MoralExemplarEthics = require('../ethics/moral-exemplar-based-ethics.js');
+const helper = require('../utils/helper.js');
+const printer = require('../utils/printer.js');
+
+const gridWorld = helper.getJson('grid-world.json');
+const agent = new GridWorldAgent(gridWorld);
+
+// The argument to the moral exemplar ethics class is a list of pairs of lists.
+// Each pair of lists is a trajectory [s_1, s_2, ..., s_n], [a_1, a_2, ..., a_n]
+// where the exemplar took action a_i when it was in state s_i
+
+const ethics = new MoralExemplarEthics([[[1],['NORTH']], [[2],['NORTH']]]);
+
+console.log('Domain');
+printer.printDomain(gridWorld, ethics);
+
+console.log('Amoral Policy');
+const amoralSolution = morality.solve(agent);
+if (amoralSolution) {
+  printer.printPolicy(amoralSolution.policy, gridWorld);
+}
+
+console.log('Moral Policy');
+const moralSolution = morality.solve(agent, ethics);
+if (moralSolution) {
+  printer.printPolicy(moralSolution.policy, gridWorld, ethics);
+}
