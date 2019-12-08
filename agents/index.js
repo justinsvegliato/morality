3618c78882899d861b42495d49696a91f7c053a6
Justin Svegliato <justin.svegliato@gmail.com>
2019-12-07 22:37:18 -0500

    Added the self-driving car agent to the package

--- agents/index.js
+++ agents/index.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const GridWorldAgent = require('./grid-world-agent.js');
+const SelfDrivingCarAgent = require('./self-driver-car-agent.js');
 
 module.exports = {
-  GridWorldAgent
+  GridWorldAgent,
+  SelfDrivingCarAgent
 };
