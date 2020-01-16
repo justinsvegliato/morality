52ec02c701aee2c6551c4bc3468996f29fe67989
Justin Svegliato <justin.svegliato@gmail.com>
2020-01-16 16:47:49 -0500

    Fixed a small bug

--- agents/index.js
+++ agents/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const GridWorldAgent = require('./grid-world-agent.js');
-const SelfDrivingCarAgent = require('./self-driver-car-agent.js');
+const SelfDrivingCarAgent = require('./self-driving-car-agent.js');
 
 module.exports = {
   GridWorldAgent,
