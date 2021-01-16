55b17968e525a78a08b8ca231bf37c41ed25b6d0
Justin Svegliato <justin.svegliato@gmail.com>
2021-01-16 14:53:07 -0500

    Implemented the starting point of the three example files

--- examples/lane-merging-domain/act-utilitarianism-example.js
+++ examples/lane-merging-domain/act-utilitarianism-example.js
@@ -1,1 +1,22 @@
-todo
+'use strict';
+
+const morality = require('../../morality.js');
+const LaneMergingAgent = require('../../agents/lane-merging-agent.js');
+const ActUtilitarianism = require('../../ethics/act-utilitarianism.js');s
+const printer = require('../../utils/printer.js');
+
+const agent = new LaneMergingAgent(5, 5);
+
+const ethics = new ActUtilitarianism();
+
+console.log('Amoral Policy');
+const amoralSolution = morality.solve(agent);
+if (amoralSolution) {
+  print(amoralSolution.policy);
+}
+
+console.log('Moral Policy');
+const moralSolution = morality.solve(agent, ethics);
+if (moralSolution) {
+  print(moralSolution.policy);
+}
