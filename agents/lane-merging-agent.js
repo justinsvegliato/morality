fefab0fa3bca60b4bc9f90791b7e69565f491592
Justin Svegliato <justin.svegliato@gmail.com>
2021-01-16 16:49:08 -0500

    Fixed some bugs

--- agents/lane-merging-agent.js
+++ agents/lane-merging-agent.js
@@ -7,16 +7,17 @@ const ACTION_DETAILS = {
 };
 
 class LaneMergingAgent {
-  constructor(num_mergers, num_mergees) {
+  constructor(num_mergers, num_mergees) {    
     this._num_merging_members = num_mergers;
     this._num_mergee_members = num_mergees;
     this._num_lanes = 2;
     // min so that we have no "open" spots on the road
-    this._num_positions = Math.min(this._mergers, this._mergees);
+    this._num_positions = Math.min(this._num_merging_members, this._num_mergee_members);
     // The 2 is because the agent could be in either lane
     // The other 2 is because the agent's lane could be moving, or not 
     // +1 for the goal state
-    this._size = this._mergers * this._mergees * 2 * 2 * this._positions + 1;
+    // console.log(this._num_merging_members * this._num_mergee_members * 2 * 2 * this.num_pos)
+    this._size = this._num_merging_members * this._num_mergee_members * 2 * 2 * this._num_positions + 1;
   }
 
   states() {
@@ -28,7 +29,7 @@ class LaneMergingAgent {
   }
 
   getStateFactorsFromState(state) {
-    state_factors = {}
+    let state_factors = {}
     if (state == this._size-1) {
       state_factors['is_goal'] = true;
       state_factors['lane_id'] = 'NONE';
@@ -36,15 +37,16 @@ class LaneMergingAgent {
       state_factors['position'] = 'NONE';
       state_factors['num_mergers_remaining'] = 'NONE';
       state_factors['num_mergees_remaining'] = 'NONE';
+    }
     else {
       state_factors['is_goal'] = false;
       state_factors['lane_id'] = 'MERGER';
       if (state < ((this._size-1) / 2)) {
         state_factors['lane_id'] = 'MERGEE';
       }
-      state = state % (this._size-1) / 2);
+      state = state % ((this._size-1) / 2);
       state_factors['is_moving'] = false;
-      if (state < (this._size-1) / 4)) {
+      if (state < ((this._size-1) / 4)) {
         state_factors['is_moving'] = true;
       }
       state = state % (state / 2);
@@ -52,15 +54,15 @@ class LaneMergingAgent {
       state = state % (this._num_merging_members * this._num_mergee_members);
       state_factors['num_mergers_remaining'] = Math.floor(state / this._num_merging_members);
       state_factors['num_mergees_remaining'] = state % this._num_merging_members;
+    }
 
     return state_factors;
   }
 
   // TODO: double check everything
   transitionFunction(state, action, successorState) {
-
-    state_factors = getStateFactorsFromState(state);
-    successor_state_factors = getStateFactorsFromState(successorState);
+    const state_factors = this.getStateFactorsFromState(state);
+    const successor_state_factors = this.getStateFactorsFromState(successorState);
    
     //TODO: figure out how to handle the position state factor...
     // self loop illegal states forever. obvi not the best way to do it... but its much easier index math this way
@@ -82,10 +84,10 @@ class LaneMergingAgent {
     }
     
     // self-loop on the goal state forever (the have-merged state)
-    if (state_factors['is_goal'] && succesor_state_factors['is_goal']) {
+    if (state_factors['is_goal'] && successor_state_factors['is_goal']) {
       return 1.0;
     }
-    if (state_factors['is_goal'] && !succesor_state_factors['is_goal']) {
+    if (state_factors['is_goal'] && !successor_state_factors['is_goal']) {
       return 0.0;
     }
     
@@ -152,13 +154,13 @@ class LaneMergingAgent {
           if ((state_factors['position'] == successor_state_factors['position'] + 1) &&
               (state_factors['num_mergers_remaining'] == successor_state_factors['num_mergers_remaining'] + 1) && 
               (state_factors['num_mergees_remaining'] == successor_state_factors['num_mergees_remaining']) && 
-              (successor_state_factors['is_moving']) {
+              (successor_state_factors['is_moving'])) {
             return 0.4;
           }
           if ((state_factors['position'] == successor_state_factors['position']) &&
               (state_factors['num_mergers_remaining'] == successor_state_factors['num_mergers_remaining']) &&
               (state_factors['num_mergees_remaining'] == successor_state_factors['num_mergees_remaining'] + 1) &&
-              (!successor_state_factors['is_moving']) {
+              (!successor_state_factors['is_moving'])) {
             return 0.6;
           }
         }
@@ -166,13 +168,13 @@ class LaneMergingAgent {
           if ((state_factors['position'] == successor_state_factors['position'] + 1) &&
               (state_factors['num_mergers_remaining'] == successor_state_factors['num_mergers_remaining'] + 1) && 
               (state_factors['num_mergees_remaining'] == successor_state_factors['num_mergees_remaining']) && 
-              (successor_state_factors['is_moving']) {
+              (successor_state_factors['is_moving'])) {
             return 0.3;
           }
           if ((state_factors['position'] == successor_state_factors['position']) &&
               (state_factors['num_mergers_remaining'] == successor_state_factors['num_mergers_remaining']) &&
               (state_factors['num_mergees_remaining'] == successor_state_factors['num_mergees_remaining'] + 1) &&
-              (!successor_state_factors['is_moving']) {
+              (!successor_state_factors['is_moving'])) {
             return 0.7;
           }
         }
@@ -182,13 +184,13 @@ class LaneMergingAgent {
           if ((state_factors['position'] == successor_state_factors['position'] + 1) &&
               (state_factors['num_mergers_remaining'] == successor_state_factors['num_mergers_remaining'] + 1) && 
               (state_factors['num_mergees_remaining'] == successor_state_factors['num_mergees_remaining']) && 
-              (successor_state_factors['is_moving']) {
+              (successor_state_factors['is_moving'])) {
             return 0.3;
           }
           if ((state_factors['position'] == successor_state_factors['position']) &&
               (state_factors['num_mergers_remaining'] == successor_state_factors['num_mergers_remaining']) &&
               (state_factors['num_mergees_remaining'] == successor_state_factors['num_mergees_remaining'] + 1) &&
-              (!successor_state_factors['is_moving']) {
+              (!successor_state_factors['is_moving'])) {
             return 0.7;
           }
         }
@@ -196,13 +198,13 @@ class LaneMergingAgent {
           if ((state_factors['position'] == successor_state_factors['position'] + 1) &&
               (state_factors['num_mergers_remaining'] == successor_state_factors['num_mergers_remaining'] + 1) && 
               (state_factors['num_mergees_remaining'] == successor_state_factors['num_mergees_remaining']) && 
-              (successor_state_factors['is_moving']) {
+              (successor_state_factors['is_moving'])) {
             return 0.4;
           }
           if ((state_factors['position'] == successor_state_factors['position']) &&
               (state_factors['num_mergers_remaining'] == successor_state_factors['num_mergers_remaining']) &&
               (state_factors['num_mergees_remaining'] == successor_state_factors['num_mergees_remaining'] + 1) &&
-              (!successor_state_factors['is_moving']) {
+              (!successor_state_factors['is_moving'])) {
             return 0.6;
           }
         }
@@ -225,7 +227,7 @@ class LaneMergingAgent {
     // double check that we don't want to add any extra dynamics here like awkward merges holding up traffic
     if (state_factors['lane_id'] == 'MERGEE') {
       if (action == 'CONTINUE') {
-        if (successor_state_factor['is_goal'] == true) {
+        if (successor_state_factors['is_goal'] == true) {
           return 1.0;
         }
         else {
@@ -252,7 +254,7 @@ class LaneMergingAgent {
         if ((state_factors['position'] == successor_state_factors['position']) &&
             (state_factors['num_mergers_remaining'] == successor_state_factors['num_mergers_remaining']) &&
             (state_factors['num_mergees_remaining'] == successor_state_factors['num_mergees_remaining'] + 1) &&
-            (!successor_state_factors['is_moving']) {
+            (!successor_state_factors['is_moving'])) {
           return 0.6;
         }
       }
@@ -263,7 +265,7 @@ class LaneMergingAgent {
         if ((state_factors['position'] == successor_state_factors['position']) &&
             (state_factors['num_mergers_remaining'] == successor_state_factors['num_mergers_remaining']) &&
             (state_factors['num_mergees_remaining'] == successor_state_factors['num_mergees_remaining'] + 1) &&
-            (!successor_state_factors['is_moving']) {
+            (!successor_state_factors['is_moving'])) {
           return 0.7;
         }
       }
@@ -271,7 +273,7 @@ class LaneMergingAgent {
     return 0.0;
   }
 
-  rewardFunction(state, action) {
+  rewardFunction(state, _) {
     if (state == this._size-1) {
       return 0.0;
     }
@@ -282,10 +284,10 @@ class LaneMergingAgent {
 
   startStates() {
     // can't start at the goal, or in a state where the position is larger than the lane
-    valid_start_states = [];
-    potential_states = [...Array(this._size-1).keys()];
-    for (state of potential_states) {
-      state_factors = getStateFactorsFromState(state);
+    let valid_start_states = [];
+    const potential_states = [...Array(this._size-1).keys()];
+    for (const state of potential_states) {
+      const state_factors = this.getStateFactorsFromState(state);
       if (state_factors['lane_id'] == 'MERGER') {
         if (state_factors['position'] <= state_factors['num_mergers_remaining']) {
           valid_start_states.push(state);
@@ -301,4 +303,4 @@ class LaneMergingAgent {
   }
 }
 
-module.exports = GridWorldAgent;
+module.exports = LaneMergingAgent;
