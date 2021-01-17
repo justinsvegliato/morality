3308ea4ba1a5076fe393a0f2567f97c770c455f1
Samer Nashed <samernashed@gmail.com>
2021-01-16 23:03:56 -0500

    valid transition function for lane merging agent

--- agents/lane-merging-agent.js
+++ agents/lane-merging-agent.js
@@ -7,17 +7,18 @@ const ACTION_DETAILS = {
 };
 
 class LaneMergingAgent {
-  constructor(num_mergers, num_mergees) {    
-    this._num_merging_members = num_mergers;
-    this._num_mergee_members = num_mergees;
+  constructor(num_mergers, num_mergees) {
+    // The +1 are because lanes may be empty
+    this._num_merging_states = num_mergers + 1;
+    this._num_mergee_states = num_mergees + 1;
     this._num_lanes = 2;
     // min so that we have no "open" spots on the road
-    this._num_positions = Math.min(this._num_merging_members, this._num_mergee_members);
+    // The -1 is because unlike merging states, we cannot have a position of '0'
+    this._num_positions = Math.min(this._num_merging_states, this._num_mergee_states) - 1;
     // The 2 is because the agent could be in either lane
     // The other 2 is because the agent's lane could be moving, or not 
     // +1 for the goal state
-    // console.log(this._num_merging_members * this._num_mergee_members * 2 * 2 * this.num_pos)
-    this._size = this._num_merging_members * this._num_mergee_members * 2 * 2 * this._num_positions + 1;
+    this._size = (this._num_merging_states) * (this._num_mergee_states) * 2 * 2 * this._num_positions + 1;
   }
 
   states() {
@@ -49,24 +50,26 @@ class LaneMergingAgent {
       if (state < ((this._size-1) / 4)) {
         state_factors['is_moving'] = true;
       }
-      state = state % (state / 2);
-      state_factors['position'] = Math.floor(state / (this._num_merging_members * this._num_mergee_members));
-      state = state % (this._num_merging_members * this._num_mergee_members);
-      state_factors['num_mergers_remaining'] = Math.floor(state / this._num_merging_members);
-      state_factors['num_mergees_remaining'] = state % this._num_merging_members;
+      state = state % ((this._size-1) / 4);
+      state_factors['position'] = Math.floor(state / (this._num_merging_states * this._num_mergee_states)) + 1;
+      state = state % (this._num_merging_states * this._num_mergee_states);
+      state_factors['num_mergers_remaining'] = Math.floor(state / this._num_merging_states);
+      state_factors['num_mergees_remaining'] = state % this._num_merging_states;
     }
 
     return state_factors;
   }
 
-  // TODO: double check everything
   transitionFunction(state, action, successorState) {
     const state_factors = this.getStateFactorsFromState(state);
     const successor_state_factors = this.getStateFactorsFromState(successorState);
-   
-    //TODO: figure out how to handle the position state factor...
+  
+    //console.log(state_factors['position']);
+    console.log(state_factors['num_mergers_remaining']);
+    console.log(state_factors['num_mergees_remaining']);
+ 
     // self loop illegal states forever. obvi not the best way to do it... but its much easier index math this way
-    if (state_factors['lane_id'] == 'MERGERS' && (state_factors['position'] > state_factors['num_mergers_remaining'])) {
+    if (state_factors['lane_id'] == 'MERGER' && (state_factors['position'] > state_factors['num_mergers_remaining'])) {
       if (state == successorState) {
         return 1.0;
       }
@@ -74,7 +77,7 @@ class LaneMergingAgent {
         return 0.0;
       }
     }
-    if (state_factors['lane_id'] == 'MERGEES' && (state_factors['position'] > state_factors['num_mergees_remaining'])) {
+    if (state_factors['lane_id'] == 'MERGEE' && (state_factors['position'] > state_factors['num_mergees_remaining'])) {
       if (state == successorState) {
         return 1.0;
       }
@@ -82,7 +85,7 @@ class LaneMergingAgent {
         return 0.0;
       }
     }
-    
+
     // self-loop on the goal state forever (the have-merged state)
     if (state_factors['is_goal'] && successor_state_factors['is_goal']) {
       return 1.0;
@@ -91,44 +94,59 @@ class LaneMergingAgent {
       return 0.0;
     }
     
+    // can't reach the goal from more than one place away
+    if ((state_factors['position'] != 1) && successor_state_factors['is_goal']) {
+      return 0.0;
+    }
+    
     // can't switch lanes   
-    if (state_factors['lane_id'] != successor_state_factors['lane_id']) {
+    if ((state_factors['lane_id'] != successor_state_factors['lane_id']) &&
+        (successor_state_factors['is_goal'] != true)) {
       return 0.0;
     }
 
     // can't go backwards
-    if (successor_state_factors['position'] > state_factors['position']) {
+    if ((successor_state_factors['position'] > state_factors['position']) &&
+        (successor_state_factors['is_goal'] != true)) {
       return 0.0;
     }
 
     // can't skip ahead more than one car at a time
-    if (state_factors['position'] - successor_state_factors['position'] > 1) {
+    if ((state_factors['position'] - successor_state_factors['position'] > 1) &&
+        (successor_state_factors['is_goal'] != true)) {
       return 0.0;
     }
 
     // can't have more than one car merge each time step
-    if (state_factors['num_mergees_remaining'] > successor_state_factors['num_mergees_remaining'] + 1) {
+    if ((state_factors['num_mergees_remaining'] > successor_state_factors['num_mergees_remaining'] + 1) &&
+        (successor_state_factors['is_goal'] != true)) {
       return 0.0;
     } 
-    if (state_factors['num_mergers_remaining'] > successor_state_factors['num_mergers_remaining'] + 1) {
+    if ((state_factors['num_mergers_remaining'] > successor_state_factors['num_mergers_remaining'] + 1) &&
+        (successor_state_factors['is_goal'] != true)) {
       return 0.0;
     } 
     
     // can't add new cars
-    if (state_factors['num_mergees_remaining'] < successor_state_factors['num_mergees_remaining']) {
+    if ((state_factors['num_mergees_remaining'] < successor_state_factors['num_mergees_remaining']) &&
+        (successor_state_factors['is_goal'] != true)) {
       return 0.0;
     } 
-    if (state_factors['num_mergers_remaining'] < successor_state_factors['num_mergers_remaining']) {
+    if ((state_factors['num_mergers_remaining'] < successor_state_factors['num_mergers_remaining']) &&
+        (successor_state_factors['is_goal'] != true)) {
       return 0.0;
     } 
 
-    if ((state_factors['position'] != 0) && !successor_state_factors['is_goal']) { 
-      
+    // TODO: need an awkwardness dynamic so that for efficiency's sake, one lane is preferred over another at a time
+
+    // Waiting dynamics
+    if (state_factors['position'] != 1) {
+
       // if merger lane is empty, traffic flow is smooth
       if (state_factors['num_mergers_remaining'] == 0) {
         if ((successor_state_factors['num_mergees_remaining'] == state_factors['num_mergees_remaining'] - 1) &&
             (successor_state_factors['position'] == state_factors['position'] - 1) && 
-            (state_factors['lane_id'] == 'MERGEE')) {
+            (state_factors['lane_id'] == 'MERGEE') && successor_state_factors['is_moving']) {
           return 1.0
         }
         else {
@@ -140,7 +158,7 @@ class LaneMergingAgent {
       if (state_factors['num_mergees_remaining'] == 0) {
         if ((successor_state_factors['num_mergers_remaining'] == state_factors['num_mergers_remaining'] - 1) &&
             (successor_state_factors['position'] == state_factors['position'] - 1) && 
-            (state_factors['lane_id'] == 'MERGER')) {
+            (state_factors['lane_id'] == 'MERGER') && successor_state_factors['is_moving']) {
           return 1.0
         }
         else {
@@ -212,10 +230,12 @@ class LaneMergingAgent {
       return 0.0;
     }
 
-    // NOTE: the only cases to get here should have position = 0
+    // NOTE: the only cases to get here should have position = 1
+    
     // if road is empty, go directly to the goal
-    if (state_factors['num_mergers_remaining'] + state_factors['num_mergees_remaining'] == 1) {
-      if (successor_state_factors['is_moving']) {
+    if ((state_factors['num_mergers_remaining'] == 0 && state_factors['lane_id'] == 'MERGEE') || 
+        (state_factors['num_mergees_remaining'] == 0 && state_factors['lane_id'] == 'MERGER')) {
+      if ((state_factors['position'] == 1) && successor_state_factors['is_goal'] == true) {
         return 1.0;
       }
       else {
@@ -223,8 +243,6 @@ class LaneMergingAgent {
       }
     }
    
-    //TODO: double check that position is used correctly / resolve what happens at position 0/1
-    // double check that we don't want to add any extra dynamics here like awkward merges holding up traffic
     if (state_factors['lane_id'] == 'MERGEE') {
       if (action == 'CONTINUE') {
         if (successor_state_factors['is_goal'] == true) {
@@ -238,7 +256,7 @@ class LaneMergingAgent {
         if ((state_factors['num_mergers_remaining'] == successor_state_factors['num_mergers_remaining'] + 1) && 
             (state_factors['num_mergees_remaining'] == successor_state_factors['num_mergees_remaining']) && 
             (!successor_state_factors['is_moving']) &&
-            (successor_state_factors['position'] == 0)) {
+            (successor_state_factors['position'] == 1)) {
           return 1.0;
         }
         else {
