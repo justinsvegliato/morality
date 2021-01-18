bac76efa2a69b814f3b5adc166af6cc7e4f8b3ad
Justin Svegliato <justin.svegliato@gmail.com>
2021-01-17 22:45:54 -0500

    Implemented the helper function without dupes

--- utils/helper.js
+++ utils/helper.js
@@ -1,41 +1,28 @@
 'use strict';
 
 function samer(agent, moralCommunity, memberStatePrior) {
-  const states = [];
-  const successorStates = [];
-  const memberStates = [];
+  const triples = [];
 
   for (const state of agent.states()) {
-    for (const action of agent.actions()) {
-      for (const member of moralCommunity) {
-        for (const memberState of member.states()) {
-          const priorProbability = memberStatePrior(state, memberState);
-          if (priorProbability == 0) {
+    for (const memberState of moralCommunity[0].states()) {
+      const priorProbability = memberStatePrior(state, memberState);
+      if (priorProbability == 0) {
+        continue;
+      } 
+      for (const successorState of agent.states()) {
+        for (const action of agent.actions()) {
+          const transitionProbability = agent.transitionFunction(state, action, successorState);
+          if (transitionProbability == 0) {
             continue;
           } 
-          for (const successorState of agent.states()) {
-            const transitionProbability = agent.transitionFunction(state, action, successorState);
-            if (transitionProbability > 0) {
-              console.log(transitionProbability)
-            }
-            if (transitionProbability == 0) {
-              continue;
-            } 
-            states.push(state);
-            successorStates.push(successorState);
-            memberStates.push(memberState);
-          }
+          triples.push([state, successorState, memberState])
+          break;
         }
       }
     }
   }
  
-  return {
-    'states': states,
-    'successorStates': successorStates, 
-    'memberStates': memberStates, 
-    'memberSuccessorStates': agent.states()
-  };
+  return triples;
 }
 
 module.exports = {
